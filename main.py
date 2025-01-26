a2532d78ef18bd936aa5cbfc4c41aa22e184c08d
Gregory Mejia <160346215+Gregory-Mejia@users.noreply.github.com>
2025-01-25 16:31:51 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+
+    Written by Gregory Mejia
+    Date: 1/25/2025
+
+'''
+
+##  Dependancies Importing  ##
+
+from classes import DNA
+from classes import RNA
+
+##  Variables  ##
+
+
+
+##  Functions  ##
+
+print(DNA("aaa"))
+print(RNA("tttaaa"))
