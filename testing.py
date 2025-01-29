6094337bd68ec72a362b29864713ab9dacf04b97
Gregory Mejia <160346215+Gregory-Mejia@users.noreply.github.com>
2025-01-28 19:38:29 -0800

    Create testing.py
    moved my testing terminal stuff to another file from another old conflicting version with actual ui stuff

---
+++ testing.py
@@ -0,0 +1,23 @@
+'''
+
+    Written by Gregory Mejia
+    Date: 1/28/2025
+
+'''
+
+##  Dependancies Importing  ##
+
+from classes import DNA
+from classes import RNA
+
+##  Variables  ##
+
+template_strand = DNA("taa gca gcg gga atc")
+rna_strand = RNA(template_strand.strand)
+
+##  Functions  ##
+
+print(template_strand)
+print(rna_strand)
+print(rna_strand.match_codons_to_amino_acids(rna_strand.mRNA))
+print(rna_strand.match_amino_acid_full(rna_strand.match_codons_to_amino_acids(rna_strand.mRNA)))
